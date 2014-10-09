cb50b8a16fce4c2b65505a71fd7d8d4ca6a73408
yfjelley <549636719@qq.com>
2014-10-09 10:22:52 +0800

    add insert data into table alex

--- rank.py
+++ rank.py
@@ -6,45 +6,55 @@ import BeautifulSoup
 import MySQLdb
 
 try:
+    '''
+    create database 
+    '''
     conn = MySQLdb.connect(host='localhost',user='root',passwd='123')
     cursor = conn.cursor()
     sql= '''create database if not exists `baidu` default character set utf8 collate utf8_general_ci;'''
     cursor.execute(sql)
     conn.select_db('baidu')
-    print "xxx"
-    sql='''create table if not exists alex(`keyword` varchar(100),`addres` varchar(100),`rank` int(3))ENGINE=InnoDB DEFAULT CHARSET=utf8;'''
+    cursor.execute('''drop table if exists alex;''')
+    sql='''create table if not exists alex(`keyword` varchar(100),`addres` varchar(100),`site` varchar(100),`rank` int(3))ENGINE=InnoDB DEFAULT CHARSET=utf8;'''
     cursor.execute(sql)
-    cursor.execute('''truncate table alex ''')
 except Exception,e:
     print e
+def writeData(sql):
+    '''
+    insert data into table
+    '''
+    try:
+        conn = MySQLdb.connect(host='localhost',user='root',passwd='123',db='baidu')
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        print sql
+        cursor.close()
+    except Exception,e:
+        print e
+        print sql
+        pass
 
 class threadScrapy(threading.Thread):
     def __init__(self,kw,num):
         threading.Thread.__init__(self)
         self.keyWord = kw
+        self.addres = '上海'
         self.url = "http://www.baidu.com/s?wd=%s%s%s"%(self.keyWord,urllib.quote('+'),'上海')
         self.thread_num = num
         self.thread_stop = False
-        self.alex = {}
+
 
     def run(self):
-        print self.url
-        print self.thread_num
+        print self.url,self.thread_num
         html = urllib.urlopen(self.url)
         soup = BeautifulSoup.BeautifulSoup(html)
         #l = soup.html.body.find('div',id='content_left').findAll('span',{'class':re.compile('^\w{5,7}$')})
         l = soup.html.body.find('div',id='content_left').findAll('span',{'class':'g'})
-        with open('html.txt','w') as f:
-            f.write(str(l).strip())
-        print len(l)
-        print l
         parten = re.compile(r'\<span\s{1}class\=\"g"\>(.+?)\;(.+?)\<\/span\>')
         for i in l:
             match=parten.match(str(i))
             if match :
-                print match.group(1)
-                self.alex.setdefault(self.keyWord,[]).append(match.group(1))
-        print self.alex
+                writeData('''insert into alex values('%s','%s','%s',%d);'''%(self.keyWord,self.addres,match.group(1),l.index(i)))
         #l = soup.findAll(attrs={"class":'VJDfES'})
 
     def stop(self):

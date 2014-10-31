0c9c89dcc181ae53cb8e598d4d25dd2eed16926b
yfjelley <549636719@qq.com>
2014-10-31 14:30:18 +0800

    threading

--- link.py
+++ link.py
@@ -2,37 +2,43 @@
 import sys,re,urllib,threading
 from pyquery import PyQuery as pq
 reload(sys).setdefaultencoding('utf-8')
-class threadLink(threading.Thread):
-    def __init__(self,kw,ft):
-       threading.Thread.__init__(self)
-       self.thread_stop = False
-       self.kw=kw
-       self.ft=ft
-       self.d = pq("http://www.baidu.com/s?wd=%s"%self.kw)
-       self.link = self.d('a[data-nolog]')
-    def run(self):
-       for i in self.link:
-             href = self.d(i).attr('href')
-             print href
-             h=urllib.urlopen('%s'%href).read()
-             html = h.decode('gb18030').encode('utf8')
-             #print html
-             m = re.search(r'%s'%self.ft,str(html))
-             if m:
-                 print self.kw,self.ft,self.link.index(i)
-       for i in range(len(self.d('.result.c-container'))):
-            x=self.d('.result.c-container:eq(%d)'%i).text()
-            print x
-            m=re.search(r'%s'%self.ft,str(x))
+def worker(kw,ft):
+    d = pq("http://www.baidu.com/s?wd=%s"%kw)
+    #获取百度快照链接
+    link = d('a[data-nolog]')
+    for i in range(len(d('.result.c-container'))):
+        #提取搜索的页面标题，如果标题中没有特征码，则打开百度快照
+        x=d('.result.c-container:eq(%d)'%i).text()
+        print x
+        m=re.search(r'%s'%ft,str(x))
+        if m:
+            print kw,ft,i
+        else:
+            href = d(link[i]).attr('href')
+            print href
+            #打开百度快照页
+            h=urllib.urlopen('%s'%href).read()
+            html = h.decode('gb18030').encode('utf8')
+            #print html
+            m = re.search(r'%s'%ft,str(html))
             if m:
-                print "xxxx"
+                 print kw,ft,link.index(i)
+
 if __name__== "__main__":
     kw="上海不孕不育检查"
     ft="长江医院"
-    th=threadLink(kw,ft)
-    th.start()
-
-         
+ 
+    for i in xrange(100):
+        while(1):
+            if threading.activeCount() <3:
+                try:
+                    t = threading.Thread(target=worker,args=(kw,ft))
+                    t.start()
+                except Exception,e:
+                    print e
+                    pass
+                break
+             
         
 
 
